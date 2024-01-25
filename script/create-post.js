30f35374cac15e5c7dab6613a13fc6f1f0c78a1e
Yohan F <yohan.ferdin@hotmail.fr>
2024-01-25 16:35:54 +0100

    started conversion from iframes to dynamic post load

--- script/create-post.js
+++ script/create-post.js
@@ -1,15 +1,38 @@
-
-import data from '../JSON/tweets.json' assert{type:'json'};
-console.log(data);
-function populate() {
-	createPost("michel","@superMicheldu93","merde ils sont a court de 86 a lidl","https://cdn.auchan.fr/media/MEDIASTEP57426383_2048x2048/B2CD/?width=1200&height=1200",45,42,1)
+async function getTweetsData() {
+	
+	const response = await fetch("../JSON/tweets.JSON");
+	const tweetsList = await response.json();
+	for (const key in tweetsList) {
+		createPost(
+		tweetsList[key].pseudo,
+		tweetsList[key].id,
+		 tweetsList[key].message,
+		 tweetsList[key].imgurl,
+		 tweetsList[key].replies,
+		 tweetsList[key].retweet,
+		tweetsList[key].like
+		)
+	}
 }
 
+function populate() {
+	let tweetsList = getTweetsData();
+	
+	createPost("michel","@superMicheldu93","merde ils sont a court de 86 a lidl","https://cdn.auchan.fr/media/MEDIASTEP57426383_2048x2048/B2CD/?width=1200&height=1200",45,42,1)
+	//createPost("michel","@superMicheldu93","merde ils sont a court de 86 a lidl","https://cdn.auchan.fr/media/MEDIASTEP57426383_2048x2048/B2CD/?width=1200&height=1200",45,42,1)
+}
+function loopJson(data) {
+	for (const key in data) {
+	}
+}
 function createPost(pseudo, id, message, imgurl, replies, retweet, likes) {
+	console.log("creating post")
+	console.log('postcreated');
 	const container = document.createElement("div");
 	container.innerHTML =
 		"<div class='row border'><div class='col-auto'><i class='bi bi-person-circle fs-profile-grand'></i></div><div class='col'><div class='row'> <div class='col d-flex flex-row justify-content-between'><p class='fw-bold'>"+pseudo+"<span class='text-muted'>"+id+"</span></p><i class='bi bi-three-dots'></i></div><div/><div class='row'><div class='col'>"+message+"</div></div><div class='row'><div class='col'><img src='"+imgurl+"'class='img-fluid'/></div></div><div class='row text-center my-2'><div class='col'><i class='bi bi-chat'></i>"+replies+"</div><div class='col'><i class='bi bi-repeat'></i>"+retweet+"</div><div class='col'><i class='bi bi-heart'></i>"+likes+"</div><div class='col'><i class='bi bi-graph-up'></i></div><div class='col'><i class='bi bi-bookmark'></i><i class='bi bi-arrow-up'></i></div></div></div></div>";
-	document.body.appendChild(container);
+	document.getElementById("fillArea").appendChild(container);
+	//document.body.appendChild(container);
 }
 // <div class="container-fluid border" style="height: 100% !important">
 // 			<div class="row">
--- script/script.js
+++ script/script.js
@@ -1,13 +1,14 @@
-var active = "post";
-function changeFrame(linkDirection) {
-    document.getElementById("frameMidBot").src = ("html/" + linkDirection + ".html");
-    document.getElementById(active).classList.remove("fw-bold");
-    document.getElementById(active).classList.remove("underlineNav");
-    active = linkDirection;
-    document.getElementById(active).classList.add("fw-bold");
-    document.getElementById(active).classList.add("underlineNav");
-}
-function resizeIframe(obj){
-    obj.style.height = 0;
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
- }
+// let active = "post";
+// function changeFrame(linkDirection) {
+//     document.getElementById("frameMidBot").src = ("html/" + linkDirection + ".html");
+//     document.getElementById(active).classList.remove("fw-bold");
+//     document.getElementById(active).classList.remove("underlineNav");
+//     active = linkDirection;
+//     document.getElementById(active).classList.add("fw-bold");
+//     document.getElementById(active).classList.add("underlineNav");
+// }
+// function resizeIframe(obj){
+//     obj.style.height = 0;
+//     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+    
+// }
